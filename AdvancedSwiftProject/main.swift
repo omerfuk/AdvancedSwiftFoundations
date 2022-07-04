48a67cdbf1a7c8ebbe8b15da5641b93b19df2495
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-04 15:40:29 +0300

    .

--- AdvancedSwiftProject/main.swift
+++ AdvancedSwiftProject/main.swift
@@ -9,8 +9,39 @@ import Foundation
 
 let classJames = MusicianClass(name: "James", age: 50, instrument: "Guitarre")
 
-print(classJames.age)
+//print(classJames.age)
 
-let structJames = MusicianStruct(name: "James", age: 50, instrument: "Guitarre")
+var structJames = MusicianStruct(name: "James", age: 50, instrument: "Guitarre")
 
-print(structJames.age)
+//print(structJames.age)
+//
+////struct immutable
+////class mutable
+
+classJames.age = 51
+
+//print(classJames.age)
+structJames.age = 51
+
+//print(structJames.age)
+
+
+//reference vs value types
+
+let copyOfClassJames = classJames
+var copyOfStructJames = structJames
+
+print(copyOfClassJames.age)
+print(copyOfStructJames.age)
+
+copyOfStructJames.age = 52
+copyOfClassJames.age = 52
+
+print(copyOfClassJames.age)
+print(copyOfStructJames.age)
+
+//Reference type -> Class
+//copy -> Same Object new reference -> 1 object 2 references
+
+//Value Type -> Struct
+//Copy -> new object -> 2 Objects
